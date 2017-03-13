32d533692e7a2aaeaeae21550bbb3300facb4e9c
Wang Chao <trampzju@gmail.com>
2017-03-13 19:35:04 +0800

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); 
  
 app.get('/', function (req, res) {
-   var ip = req.ip.match(/\d+\.\d+\.\d+\.\d+/);
-   var lang = req.get('accept-language').substr(0, 5);
-   var reg = /\([^\)]*\)/
-   //console.log(req.get('user-agent'));
-   var info = reg.exec(req.get('user-agent'));
-   res.send("{\"ip\": " + ip + ", \"language\": " + lang + ", \"software\": " + info + "}");
+  var data = fs.readFileSync('shorturl.txt');
+  var arr = data.toString().split(";");
+  var str = "";
+  for (index in arr) {
+    str += "<br>" + index + ": " + arr[index];
+  }
+  res.send(str);
+});
+
+app.get('/new/*', function (req, res) {
+  var url = req.params[0];
+  var reg = /^http(s)?:\/\/(www\.)?(\w+\.){1}(\w+){1}$/;
+  if (!reg.test(url)) {
+    res.send("invalid url!");
+    return;
+  }
+  //console.log(url);
+  var data = fs.readFileSync('shorturl.txt');
+  var arr = data.toString().split(";");
+  arr.push(url);
+  fs.writeFile('shorturl.txt', arr.join(";"), function(err) {
+    if (err) throw err;
+  });
+  var str = "";
+  for (index in arr) {
+    str += "<br>" + index + ": " + arr[index];
+  }
+  res.send(str);
+});
+
+app.get('/*', function (req, res) {
+  var data = fs.readFileSync('shorturl.txt');
+  var arr = data.toString().split(";"); 
+  var index = parseInt(req.params[0]);
+  console.log(index);
+  if (index < arr.length) {
+    var url = arr[req.params[0]];
+    res.redirect(url);
+  }
+  else {
+    res.send('This short url is invalid!');
+  }
 });
  
 var server = app.listen(process.env.PORT || 5000, function () {
