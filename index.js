25acc6a6e2fac575feb44bb5dfdde580d6b69684
Wang Chao <trampzju@gmail.com>
2017-03-15 10:07:32 +0800

    guide

--- index.js
+++ index.js
@@ -36,6 +36,11 @@ app.get('/logs', function (req, res) {
   var str = "Latest searchs:<br>" + arr.join("<br>");
   res.send(str);
 });
+
+app.get('/', function (req, res) {
+  var str = '1. Use path "imgsearch" like "https://gentle-taiga-46841.herokuapp.com/imgsearch/cat" to search images related to query. <br> 2. Use path "logs" like "https://gentle-taiga-46841.herokuapp.com/logs" to see the log of searching activities.'
+  res.send(str);
+}
  
 var server = app.listen(process.env.PORT || 5000, function () {
  
